a2702b1e6d7e1158adc8a5cf40260978c8d9cb8b
d0xkb <d0xkb@protonmail.ch>
2016-10-30 21:45:38 +0100

    method of creating key changed

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -4,16 +4,18 @@
 key="/root/.ssh/authorized_keys"
 perm="600"
 
-# install publis ssh key and set permissions
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO273ebwYHLk/mhuXeNIqCtef3xa1iznD+EU81tjZJys d0xkb@d0xkbs-MacBook-Air.local" > $key
+# install ssh key and set permissions
+cat > $key <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO273ebwYHLk/mhuXeNIqCtef3xa1iznD+EU81tjZJys
+EOF
 
-# check permissions and correct if needed
+# check ssh key permissions and correct them if needed
 if [[ $(stat -c %a "$key") != $perm ]]; then
   chmod $perm $key
 fi
 
-# disable password authentification
+# disable password authentification and restart ssh service
 if [[ $(sshd -T |grep passwordauth |awk '{print $2}') != no ]]; then
   sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-  systemctl restart sshd.service
+  systemctl restart ssh.service
 fi
